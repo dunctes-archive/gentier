267a57eb9d03d8af97b9f6d715ea1ddaf9176d70
Duncan Sterken <duncan.sterken123@gmail.com>
2019-08-23 14:58:50 +0200

    Fix bugs

--- src/main/java/me/duncte123/gentier/GenTier.java
+++ src/main/java/me/duncte123/gentier/GenTier.java
@@ -1,16 +1,49 @@
 package me.duncte123.gentier;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class GenTier extends JavaPlugin {
     @Override
     public void onEnable() {
-        getLogger().info("onEnable is called!");
-
         this.saveDefaultConfig();
-    }
-    @Override
-    public void onDisable() {
-        getLogger().info("onDisable is called!");
+
+        getLogger().info("Loading config");
+
+        FileConfiguration config = this.getConfig();
+        String defaultTier = config.getString("defaultTier");
+        String message = config.getString("message");
+
+        List<Map<?, ?>> permsList = config.getMapList("permissions");
+
+        /*
+         * Map(perm_name, tier)
+         */
+        Map<String, String> tierMap = new HashMap<>();
+
+        for (Map<?, ?> permMap : permsList) {
+            if (!permMap.containsKey("perm")) {
+                getLogger().warning("Found tier without perm in list, skipping");
+                continue;
+            }
+
+            if (!permMap.containsKey("tier")) {
+                getLogger().warning("Found perm without tier in list, skipping");
+                continue;
+            }
+
+            String perm = String.valueOf(permMap.get("perm"));
+            String tier = String.valueOf(permMap.get("tier"));
+
+            tierMap.put(perm, tier);
+        }
+
+        getLogger().info("Loaded all tiers");
+
+        this.getCommand("gentier").setExecutor(new GenTierCommand(tierMap, defaultTier, message));
     }
 }
---
+++ src/main/java/me/duncte123/gentier/GenTierCommand.java
@@ -0,0 +1,60 @@
+package me.duncte123.gentier;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class GenTierCommand implements CommandExecutor {
+    private final Map<String, String> tierMap;
+    private final String defaultTier;
+    private final String message;
+    private final List<String> permsList;
+
+    GenTierCommand(Map<String, String> tierMap, String defaultTier, String message) {
+        this.tierMap = tierMap;
+        this.defaultTier = defaultTier;
+        this.message = message;
+
+        this.permsList = new ArrayList<>(tierMap.keySet());
+//        Collections.reverse(this.permsList);
+
+        System.out.println(this.permsList);
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            return false;
+        }
+
+        Player player = (Player) sender;
+
+        for (String perm : this.permsList) {
+            if (player.hasPermission(perm)) {
+                this.sendFormattedMessage(player, this.tierMap.get(perm));
+
+                return true;
+            }
+        }
+
+        this.sendFormattedMessage(player, this.defaultTier);
+
+        return true;
+    }
+
+    private void sendFormattedMessage(Player player, String tier) {
+        final String message = ChatColor.translateAlternateColorCodes('&',
+                this.message.replaceAll("\\{tier}", tier)
+                        .replaceAll("\\{name}", player.getDisplayName())
+        );
+
+        player.sendMessage(message);
+    }
+}
